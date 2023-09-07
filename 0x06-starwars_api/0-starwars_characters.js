330a27a5c407da2b5a0ca89fa9287fe5de742b78
Kimberley-saka <kimberleysaka@gmail.com>
2023-09-07 03:33:43 -0700

    added a few things

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-/*get movie characters*/
 import request from 'request';
 
 const movieId = process.argv[2];
-
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
+if (!movieId) {
+  console.error('please provide movie id');
+  process.exit(1);
+}
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
